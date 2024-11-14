425d04f3afb0af538d1e1a421dbd9578fdee0503
Bavaria1222 <129691586+Bavaria1222@users.noreply.github.com>
2024-11-13 21:26:34 -0600

    inittial commit

---
+++ src/main/java/com/example/proyecto_2_paradigmas/Proyecto2ParadigmasApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_2_paradigmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2ParadigmasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyecto2ParadigmasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyecto_2_paradigmas/Proyecto2ParadigmasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_2_paradigmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2ParadigmasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
